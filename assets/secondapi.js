8367b908b2eebabe73ce68ea0e5c25d78135a7ac
Grant Royseth <groyseth@gmail.com>
2022-01-07 10:49:05 -0600

    working on second api

--- assets/secondapi.js
+++ assets/secondapi.js
@@ -1,10 +1,9 @@
 var imageFood = document.querySelector("#img1");
 var API1 =  "bc3600e23d5e465f999caa4b7e68f31a"
 var API2 = "4205dfaac5c8485eb2c6e53fe9758c5c"
-
-
+var API3 = "1234"
 function randomFood(foodSearch) {
-    fetch("https://api.spoonacular.com/food/menuItems/search?query=" + foodSearch + "&number=1&apiKey=" + API1)//add API KEY when needed
+    fetch("https://api.spoonacular.com/food/menuItems/search?query=" + foodSearch + "&number=1&apiKey="+ API3 )//add API KEY when needed
         .then(function (response) {
             if (response.ok) {
                 fetchStatus = "goodFood";
@@ -12,22 +11,38 @@ function randomFood(foodSearch) {
                 return response.json();
             } else {
                 fetchStatus = "badFood";
+                console.log("bad")
+                
+
+                
             }
         })
         .then(function (data) {
            
-            if(data.expires){
+            if(data){
                 
                 // console.log(data)
-                
-                console.log(data.menuItems[0])
+                var pic = data
+                // console.log(data.menuItems[0])
                
-                $("#img1").attr("src",  data.menuItems[0].image);
-                
+               pic = $("#img1").attr("src",  data.menuItems[0].image);
+                console.log(pic)
             }
         })
+        // .catch(function(err) {
+        //     console.log('Error: ' + err);
+        //   });
 }
 
+  
+  // Now call the function inside fetch promise resolver
+//   fetch("https://api.spoonacular.com/food/menuItems/search?query=" + foodSearch + "&number=1&apiKey=" + API2  )
+//     .then(CheckError)
+//     .then((jsonResponse) => {
+//     }).catch((error) => {
+//     });
+
+    
 function getInputValue(e) {
     e.preventDefault()
     // Selecting the input element and get its value 
