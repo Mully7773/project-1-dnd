03f53972c1b9e384b0ebe139d5f34c5c32be48d5
Grant Royseth <groyseth@gmail.com>
2022-01-07 11:42:51 -0600

    working on secondapi

--- assets/secondapi.js
+++ assets/secondapi.js
@@ -1,46 +1,53 @@
 var imageFood = document.querySelector("#img1");
-var API1 =  "bc3600e23d5e465f999caa4b7e68f31a"
+var API1 = "bc3600e23d5e465f999caa4b7e68f31a"
 var API2 = "4205dfaac5c8485eb2c6e53fe9758c5c"
-var API3 = "1234"
+var inputVal;
+
 function randomFood(foodSearch) {
-    fetch("https://api.spoonacular.com/food/menuItems/search?query=" + foodSearch + "&number=1&apiKey="+ API3 )//add API KEY when needed
+    fetch("https://api.spoonacular.com/food/menuItems/search?query=" + foodSearch + "&number=1&apiKey=123345456" )//add API KEY when needed
         .then(function (response) {
             if (response.ok) {
                 fetchStatus = "goodFood";
-
                 return response.json();
-            } else {
-                fetchStatus = "badFood";
-                console.log("bad")
-                
-
-                
+            }else{
+                fetchStatus = "bad"
+                backUp(inputVal);
             }
+            
         })
         .then(function (data) {
-           
-            if(data){
-                
-                // console.log(data)
+            if(data){   
                 var pic = data
-                // console.log(data.menuItems[0])
-               
-               pic = $("#img1").attr("src",  data.menuItems[0].image);
-                console.log(pic)
-            }
-        })
-        // .catch(function(err) {
-        //     console.log('Error: ' + err);
-        //   });
+                pic = $("#img1").attr("src",  data.menuItems[0].image);
+                console.log(data)
+           
+        }
+    })
+}
+function  backUp(inputVal) {
+    
+
+    fetch("https://api.spoonacular.com/food/menuItems/search?query=" + inputVal + "&number=1&apiKey=" + API2 )
+    .then(function (response) {
+        if (response.ok) {
+            fetchStatus = "goodFood";
+            return response.json();
+        }else{
+            
+            fetchStatus = "bad"
+        }
+    })
+    .then(function (data) {
+        if(data){   
+            var pic = data
+          pic =  $("#img1").attr("src",  data.menuItems[0].image);
+            console.log(data)
+        }
+    })
 }
 
-  
-  // Now call the function inside fetch promise resolver
-//   fetch("https://api.spoonacular.com/food/menuItems/search?query=" + foodSearch + "&number=1&apiKey=" + API2  )
-//     .then(CheckError)
-//     .then((jsonResponse) => {
-//     }).catch((error) => {
-//     });
+
+
 
     
 function getInputValue(e) {
@@ -48,7 +55,11 @@ function getInputValue(e) {
     // Selecting the input element and get its value 
     var inputVal = document.getElementById("foodSearch").value;
 
-    randomFood(inputVal);
+    randomFood(inputVal)
+    if(randomFood = false){
+        backUp(inputVal)
+    }
+    
     console.log(inputVal)
 }
 document.querySelector(".subBtn").addEventListener("click", getInputValue)
